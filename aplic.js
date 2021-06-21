3ddda1fca0e3ccdde7edbf31aa5499b6011e4dd3
leonardo-farias <leonardo.silvadefarias@gmail.com>
2021-06-21 19:58:31 -0300

    att

---
+++ aplic.js
@@ -0,0 +1,4 @@
+var nomeMercadoria = <input type = "text" class = "div-select" placeholder = "Input"> </input>
+
+var valorMercadoria = <input type = "number" placeholder = "R$ 0,00" class = "div-select" > </input>
+
